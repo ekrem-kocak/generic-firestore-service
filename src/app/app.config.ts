dab107b5497d659b15b87a971e715c7a47f3ea2d
ekrem-kocak <ekrem-kocak@outlook.com>
2024-11-04 23:05:54 +0300

    - created FirestoreService - fixed InjectionToken angularfire2.app.options error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,13 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebaseOptions)),
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseOptions },
     provideFirestore(() => getFirestore()),
   ],
 };
